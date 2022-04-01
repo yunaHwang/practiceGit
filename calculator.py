5126872ba5ab00e5982a817cc9a20684c48553c9
yuna <rdking9812@gmail.com>
2022-04-01 19:43:09 +0900

    Add get_Sum_ver2 function

--- calculator.py
+++ calculator.py
@@ -14,5 +14,8 @@ def divide_new(a,b):
 def get_Median(a,b):
     return (a+b)/2
 
-def get_Sum_ver1(n):
-    return n(n+1)/2
+def get_Sum_ver2(n):
+    sum = 0
+    for i in range(1,n+1):
+        sum = sum + i 
+    return sum
